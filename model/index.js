bb9f3efe3d78d243a7cf828d86bbbb1b5e755eb1
AlinaPristinskaya <pristinskayaalina9@gmail.com>
2021-10-03 02:12:12 +0300

    add and pul

--- model/index.js
+++ model/index.js
@@ -1,15 +1,40 @@
 // const fs = require('fs/promises')
 // const contacts = require('./contacts.json')
+const crypto=require('crypto')
+const DB= require('./db')
+const db= new DB('contacts.json')
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  return await db.read()
+}
 
 const getContactById = async (contactId) => {}
 
 const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  const contacts= await db.read()
+  if(!body.name || !body.email || !body.phone){
+  return ({"message": "missing required name field"})  
+  }
+  const newContact={id:crypto.randomUUID(),...body}
+  contacts.push(newContact)
+  await db.write(contacts)
+  return newContact
 
-const updateContact = async (contactId, body) => {}
+}
+
+const updateContact = async (contactId, body) => {
+  const contacts= await db.read()
+  const index=contacts.findIndex((contact)=>contact.id===contactId)
+  if(index!==-1){
+    const contact= contacts[index]
+    contacts[index]={...contact,...body}
+    await db.write(contacts)
+    return contacts[index]
+  }
+  return null
+}
 
 module.exports = {
   listContacts,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const Contacts= require('../../model')
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try{
+    const contacts= await Contacts.listContacts()
+    res.json({ status: 'success', code: 200, data: {contacts} })
+  }catch(error){
+    next(error)
+  }
+  
 })
 
 router.get('/:contactId', async (req, res, next) => {
@@ -10,7 +17,16 @@ router.get('/:contactId', async (req, res, next) => {
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try{
+    const contact= await Contacts.addContact(req.body)
+    if (!contact.name||!contact.email||!contact.phone){
+      return res.status(400).json({status:'error', code:400,data:{contact}})
+    }
+    res.status(201).json({ status: 'success', code: 201, data: {contact} })
+    
+  }catch(error){
+    next(error)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
@@ -19,6 +35,26 @@ router.delete('/:contactId', async (req, res, next) => {
 
 router.patch('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
+})
+
+router.put('/:contactId', async (req, res, next) => {
+  try{
+    const body= (req.body.name||req.body.email||req.body.phone)
+    if(!body){
+      return res.status(400).json({status:'error', code:400, message:"missing fields"})
+
+    }    
+      const contact= await Contacts.updateContact(req.params.contactId, req.body)
+      if (contact){
+      return res.status(201).json({ status: 'success', code: 201, data: {contact} })
+
+    } return res.status(404).json({status:'error', code:404, message:"Not found"})
+
+    
+    
+  }catch(error){
+    next(error)
+  }
 })
 
 module.exports = router
